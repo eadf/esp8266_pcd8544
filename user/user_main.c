6d2aaa8d6e8ba3ffaed77d9b571f571c71d6a1e6
EAD Fritz <ead.fritz@gmail.com>
2015-01-16 23:47:43 +0100

    fixed a compile error

--- user/user_main.c
+++ user/user_main.c
@@ -5,7 +5,7 @@
 #include "gpio.h"
 #include "osapi.h"
 #include "os_type.h"
-#include "include/user_config.h"
+#include "user_config.h"
 #include "user_interface.h"
 #include "driver/stdout.h"
 #include "driver/pcd8544.h"
@@ -29,55 +29,41 @@ loop(os_event_t *events) {
     PCD8544_lcdClear();
     firstTime = false;
   }
-
-  if (true){
-    // Draw a Box
-    PCD8544_drawLine();
-    int a=0;
-    PCD8544_gotoXY(17,1);
-    // Put text in Box
-    PCD8544_lcdString("ESP8266");
-    PCD8544_gotoXY(24,3);
-    if (toggle){
-      PCD8544_lcdCharacter('H');
-      PCD8544_lcdCharacter('E');
-      PCD8544_lcdCharacter('L');
-      PCD8544_lcdCharacter('L');
-      PCD8544_lcdCharacter('O');
-      PCD8544_lcdCharacter(' ');
-      PCD8544_lcdCharacter('=');
-      // Draw + at this position
-      PCD8544_gotoXY(10,3);
-      PCD8544_lcdCharacter('=');
-      os_delay_us(50000);
-    } else {
-      PCD8544_gotoXY(24,3);
-      PCD8544_lcdCharacter('h');
-      PCD8544_lcdCharacter('e');
-      PCD8544_lcdCharacter('l');
-      PCD8544_lcdCharacter('l');
-      PCD8544_lcdCharacter('o');
-      PCD8544_lcdCharacter(' ');
-      PCD8544_lcdCharacter('-');
-      // Draw - at this position
-      PCD8544_gotoXY(10,3);
-      PCD8544_lcdCharacter('-');
-      os_delay_us(50000);
-    }
-    toggle = !toggle;
-  } else {
-    os_delay_us(50000);
-    PCD8544_lcdClear();
-    os_delay_us(50000);
-    PCD8544_gotoXY(24,3);
+  os_printf("Updating display\n\r");
+  // Draw a Box
+  PCD8544_drawLine();
+  int a=0;
+  PCD8544_gotoXY(17,1);
+  // Put text in Box
+  PCD8544_lcdString("ESP8266");
+  PCD8544_gotoXY(24,3);
+  if (toggle){
     PCD8544_lcdCharacter('H');
+    PCD8544_lcdCharacter('E');
+    PCD8544_lcdCharacter('L');
+    PCD8544_lcdCharacter('L');
+    PCD8544_lcdCharacter('O');
+    PCD8544_lcdCharacter(' ');
+    PCD8544_lcdCharacter('=');
+    // Draw + at this position
+    PCD8544_gotoXY(10,3);
+    PCD8544_lcdCharacter('=');
     os_delay_us(50000);
-    PCD8544_drawLine();
-    PCD8544_gotoXY(4,1);
-    // Put text in Box
-    PCD8544_lcdString("ESP8266");
+  } else {
+    PCD8544_gotoXY(24,3);
+    PCD8544_lcdCharacter('h');
+    PCD8544_lcdCharacter('e');
+    PCD8544_lcdCharacter('l');
+    PCD8544_lcdCharacter('l');
+    PCD8544_lcdCharacter('o');
+    PCD8544_lcdCharacter(' ');
+    PCD8544_lcdCharacter('-');
+    // Draw - at this position
+    PCD8544_gotoXY(10,3);
+    PCD8544_lcdCharacter('-');
     os_delay_us(50000);
   }
+  toggle = !toggle;
   os_timer_disarm(&lcd_timer);
   os_timer_arm(&lcd_timer, user_procTaskPeriod, 0);
 }
